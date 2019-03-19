4274da59bf44fac6778963ac9418b07b30e50da1
Mareen <44037844+Mareen82@users.noreply.github.com>
2019-03-19 13:57:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 $(document).ready(function(){
-var magic8Ball = {};
+	var magic8Ball = {};
 
-magic8Ball.listOfAnswers = ["No", "Yes", "Hell yes!", "I don't think so...", "Of course!", "What do you think?", "Maybe tomorrow", "Be more specific...", "Sources say no", "Sources say yes"];
+	magic8Ball.listOfAnswers = ["No", "Yes", "Hell yes!", "I don't think so...", "Of course!", "What do you think?", "Maybe tomorrow", "Be more specific...", "Sources say no", "Sources say yes"];
 
-magic8Ball.askQuestion = function(question){
-	var randomNumber = Math.random();
-    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
-    var randomIndex = Math.floor(randomNumberForListOfAnswers);
-    var answer = this.listOfAnswers[randomIndex];
+	magic8Ball.askQuestion = function(question){
+    		var randomNumber = Math.random();
+    		var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+    		var randomIndex = Math.floor(randomNumberForListOfAnswers);
+    		var answer = this.listOfAnswers[randomIndex];
 
-$("#8ball").effect("shake");
-$("#answer").text(answer);
-$("#answer").fadeIn(4000);
-
+	$("#8ball").effect("shake");
+	$("#answer").text(answer);
+	$("#answer").fadeIn(4000);
 
 	console.log(question);
-    console.log(answer);
+	console.log(answer);
 };
 
 var onClick = function() {
 
-$("#answer").hide();
-$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-
-
-var questionButton = prompt("Give me a yes/no question");
-
-
-magic8Ball.askQuestion(questionButton);
-
-   };
+	$("#answer").hide();
+	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+		var questionButton = prompt("Give me a yes/no question");
+	magic8Ball.askQuestion(questionButton);
+};
 
 $("#questionButton").click(onClick);
-
-
-	});
+});
